050dfe9d06d7a9f9fce1a8f7dab601cf2e803905
malk-carlos <81245539+malk-carlos@users.noreply.github.com>
2022-10-14 02:03:25 +0900

    update

--- src/components/Access.js
+++ src/components/Access.js
@@ -2,12 +2,39 @@
 import './Access.css';
 
 function Access() {
-    const adress = "〒730-0051 広島県広島市中区大手町３丁目１−９ 鯉城広島サンケイビル４階";
+    const adress = '〒730-0051 広島県広島市中区大手町3丁目1-9 鯉城広島サンケイビル4階';
+    // 埋め込み地図のソース
+    const map_src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3292.4807299585036!2d132.45334231321408!3d34.38912510075575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x355aa33ded923237%3A0x7702bca58f918468!2zTumrmOetieWtpuagoSDvvaVT6auY562J5a2m5qChIOW6g-WztuOCreODo-ODs-ODkeOCue-9nOmAmuS_oeWItumrmOagoQ!5e0!3m2!1sja!2sjp!4v1665245446974!5m2!1sja!2sjp'
+    // 広電ページurl
+    const tra_src = 'https://www.hiroden.co.jp/train/route-guide/stop-guide/u-3.html';
+    // バス路線掲載ページurl （ナビタイム）
+    const bus_src ='https://www.navitime.co.jp/bus/diagram/direction/00093111/';
+    
     return (
         <div id="Access" className='section'>
             <h1 className='sec-title'>会場アクセス</h1>
-            <p>{adress}</p>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3292.4807299585036!2d132.45334231321408!3d34.38912510075575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x355aa33ded923237%3A0x7702bca58f918468!2zTumrmOetieWtpuagoSDvvaVT6auY562J5a2m5qChIOW6g-WztuOCreODo-ODs-ODkeOCue-9nOmAmuS_oeWItumrmOagoQ!5e0!3m2!1sja!2sjp!4v1665245446974!5m2!1sja!2sjp" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div id='area-map'>
+                <iframe src={map_src} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div id='texts'>
+                <h2>N高等学校・S高等学校 広島キャンパス</h2>
+                <p>{adress}</p>
+
+                {/* ここのまとめ方もう少しスマートにしたい */}
+                <p id='nearest'>最寄駅</p>
+                <div id='train' className='ways'>
+                    <h3><a href={tra_src} target='_blank' rel='noopener noreferrer'>中電前（路面電車）</a></h3>
+                    <p><span id='fir'>●</span>1号線 &#091;広島駅 - 広島港&#093;</p>
+                    <p><span id='thi'>●</span>3号線 &#091;広電西広島 - 広島港&#093;</p>
+                    <p><span id='sev'>●</span>7号線 &#091;横川駅 - 広電本社前&#093;</p>
+                </div>
+                <div id='bus' className='ways'> {/* バス路線多すぎるので路面電車のように停車する路線を表記するのは非現実的 */}
+                    <h3><a href={bus_src} target='_blank' rel='noopener noreferrer'>中電前（バス）</a></h3>
+                </div>
+                <div id='hondori' className='ways'> {/* 「本通駅（アストラムライン）」だけだと味気ない */}
+                    <h3>本通駅（アストラムライン）</h3>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Program.js
+++ src/components/Program.js
@@ -9,51 +9,57 @@ function Program() {
             <h1 className='sec-title'>企画一覧</h1>
             <div id='stage' className='pgGourp'>
                 <h2>ステージ企画</h2>
-                <div className='program'>
-                    <h3>ドMズ</h3>
-                    <img src='https://ca.slack-edge.com/T0HTW2RH7-U03AY9YFEDQ-a66a4d294769-512' />
-                    <p>TA山本竜也を擁する新進気鋭のバンドです</p>
-                </div>
-                <div className='program'>
-                    <h3>ドSズ</h3>
-                    <img src='https://ca.slack-edge.com/T0HTW2RH7-USW2A22GJ-a7a4738e02e3-512' />
-                    <p>元TA寺岡純を擁する新進気鋭のバンドです</p>
-                </div>
-                <div className='program'>
-                    <h3>T.A.Ravolution</h3>
-                    <img src='https://ca.slack-edge.com/T0HTW2RH7-UUV9FP630-0f0a8f79ef2b-512' />
-                    <p>あの伝説のバンドT.A.Ravolutionが1日限りの奇跡の復活！名曲マリーゴールドを生演奏！</p>
+                <div className='pgWrap'>
+                    <div className='program'>
+                        <h3>ドMズ</h3>
+                        <img src='https://ca.slack-edge.com/T0HTW2RH7-U03AY9YFEDQ-a66a4d294769-512' />
+                        <p>TA山本竜也を擁する新進気鋭のバンドです</p>
+                    </div>
+                    <div className='program'>
+                        <h3>ドSズ</h3>
+                        <img src='https://ca.slack-edge.com/T0HTW2RH7-USW2A22GJ-a7a4738e02e3-512' />
+                        <p>元TA寺岡純を擁する新進気鋭のバンドです</p>
+                    </div>
+                    <div className='program'>
+                        <h3>T.A.Ravolution</h3>
+                        <img src='https://ca.slack-edge.com/T0HTW2RH7-UUV9FP630-0f0a8f79ef2b-512' />
+                        <p>あの伝説のバンドT.A.Ravolutionが1日限りの奇跡の復活！名曲マリーゴールドを生演奏！</p>
+                    </div>
                 </div>
             </div>
             <div id='campus' className='pgGourp'>
                 <h2>会場企画（常設）</h2>
-                <div className='program'>
-                    <h3>射的</h3>
-                    <img src='https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png' />
-                    <p>今年こそいっぱい撃とう！</p>
-                </div>
-                <div className='program'>
-                    <h3>hoge</h3>
-                    <img src='https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png' />
-                    <p>hogehoge</p>
+                <div className='pgWrap'>
+                    <div className='program'>
+                        <h3>射的</h3>
+                        <img src='https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png' />
+                        <p>今年こそいっぱい撃とう！</p>
+                    </div>
+                    <div className='program'>
+                        <h3>hoge</h3>
+                        <img src='https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png' />
+                        <p>hogehoge</p>
+                    </div>
                 </div>
             </div>
             <div id='broadcast' className='pgGourp'>
                 <h2>配信企画</h2>
-                <div className='program'>
-                    <h3>【脱出】閉じ込められた村尾</h3>
-                    <img src='https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png' />
-                    <p>脱出できるのか</p>
-                </div>
-                <div className='program'>
-                    <h3>hoge</h3>
-                    <img src='https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png' />
-                    <p>hogehoge</p>
-                </div>
-                <div className='program'>
-                    <h3>hoge</h3>
-                    <img src='https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png' />
-                    <p>hogehoge</p>
+                <div className='pgWrap'>
+                    <div className='program'>
+                        <h3>【脱出】閉じ込められた村尾</h3>
+                        <img src='https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png' />
+                        <p>脱出できるのか</p>
+                    </div>
+                    <div className='program'>
+                        <h3>hoge</h3>
+                        <img src='https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png' />
+                        <p>hogehoge</p>
+                    </div>
+                    <div className='program'>
+                        <h3>hoge</h3>
+                        <img src='https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png' />
+                        <p>hogehoge</p>
+                    </div>
                 </div>
             </div>
         </div>

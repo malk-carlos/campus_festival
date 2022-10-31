cc8976d555f4df46572de2711d8fd42bf41e5866
malk-carlos <81245539+malk-carlos@users.noreply.github.com>
2022-11-01 00:17:48 +0900

    update_Map
    場内マップ更新

--- src/components/Map.js
+++ src/components/Map.js
@@ -11,13 +11,58 @@ function Map() {
                 <img src={Mapimg} />
             </div>
             <div id='captions'>
-                <div id='super_stage'>
-                    <h2><span>●</span>超ステージ</h2>
-                    <p>生徒によるバンド演奏や演劇などを予定しています。</p>
+                <div id='free_area'>
+                    <h2>無制限エリア</h2>
+                    <p style={{margin: 'auto auto 2vmin'}}>来場者の皆さんが自由に出入りできるエリアです。</p>
+                    <div id='super_front'>
+                        <h3><span style={{color: '#80ebed'}}>●</span>超受付</h3>
+                        <p>ここで入場チケットを提示し、リストバンドを受け取ります。検温と手指の消毒にご協力ください。</p>
+                    </div>
+                    <div id='super_local'>
+                        <h3><span style={{color: '#7061f6'}}>●</span>超企画エリア</h3>
+                        <p>来場者のみなさんが実際に参加して楽しめる企画を準備しています。</p>
+                    </div>
+                    <div id='super_wacth'>
+                        <h3><span style={{color: '#efec81'}}>●</span>超観覧</h3>
+                        <p>ステージを観覧するエリアです。</p>
+                    </div>
+                    <div id='super_photo'>
+                        <h3><span style={{color: '#cbeb70'}}>●</span>超写真エリア</h3>
+                        <p>記念撮影に特化した映えエリアです。</p>
+                    </div>
+                    <div id='super_local'>
+                        <h3><span style={{color: '#ff84d2'}}>●</span>超休憩場</h3>
+                        <p>楽しみ疲れたらここで休めます。ステージの様子を見れるモニター付きです。</p>
+                    </div>
                 </div>
-                <div id='super_wacth'>
-                    <h2><span>●</span>超観覧</h2>
-                    <p>ステージを観覧するエリアです。</p>
+                
+                <div id='restrictive_area'>
+                    <h2>制限エリア</h2>
+                    <p style={{margin: 'auto auto 2vmin'}}>入場権限のある者のみが出入りできるエリアです。対応するパスを見える位置に提げてください。</p>
+                    <div id='super_stage'>
+                        <h3><span style={{color: '#f4c168'}}>●</span>超ステージ</h3>
+                        <p>生徒によるバンド演奏や演劇などを予定しています。</p><p className='group'>（権限：実行委員,メディア,配信,技術,出演者,職員,ステージ）</p>
+                    </div>
+                    <div id='super_side'>
+                        <h3><span style={{color: '#f66461'}}>●</span>超袖</h3>
+                        <p>ステージ袖です。</p><p className='group'>（権限：実行委員,メディア,配信,技術,出演者,職員,ステージ）</p>
+                    </div>
+                    <div id='super_tech'>
+                        <h3><span style={{color: '#d367f6'}}>●</span>超技術</h3>
+                        <p>技術担当者の持ち場です。機材等が置いてあります。</p><p className='group'>（権限：実行委員,メディア,技術）</p>
+                    </div>
+                    <div id='super_stream'>
+                        <h3><span style={{color: '#d367f6'}}>●</span>超配信</h3>
+                        <p>配信担当者の持ち場です。機材等が置いてあります。</p><p className='group'>（権限：実行委員,メディア,配信）</p>
+                    </div>
+                    <div id='super_back'>
+                        <h3><span style={{color: '#5e5d90'}}>●</span>超控室</h3>
+                        <p>ステージ出演者の控え室です。</p><p className='group'>（権限：実行委員,出演者）</p>
+                    </div>
+                    <div id='super_local_back'>
+                        <h3><span style={{color: '#7061f6'}}>●</span>超企画エリアバックヤード</h3>
+                        <p>超企画エリアのバックヤードです。機材等が置いてあります。</p><p className='group'>（権限：実行委員,メディア,職員,スタッフ）</p>
+                    </div>
                 </div>
                 {/* 各箇所の名前、そこで行われること、入場規制の有無、観覧の可否 */}
             </div>

0b313339d380c4bbdc1ade186030e1eb3ec43b69
malk-carlos <81245539+malk-carlos@users.noreply.github.com>
2022-11-04 02:56:35 +0900

    update
    問い合わせページの骨組み完成
    注意事項追記

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,10 +23,10 @@ function Foot() {
                     <li><a href='../contact.html' target='_blank' rel='noopener noreferrer'>お問い合わせ</a></li>
                 </ul>
             </nav>
-            <div id='disclaimer'>
+            {/* <div id='disclaimer'>
                 <p>このサイトは生徒が運営しています。掲載する情報には細心の注意を払っておりますが、より正確な情報は<a href='https://nnn.ed.jp/' target='_blank' rel='noopener noreferrer'>N/S高等学校公式サイト</a>をご覧ください。</p>
                 <p>このサイトを閲覧したことによって閲覧者に生じたいかなる損失についても、運営者は一切の責任を負いかねます。</p>
-            </div>
+            </div> */}
             <p id='copy'>&copy; Campus Festival 2022 in Hiroshima, Malk is All rights reserved.</p>
         </footer>
     );

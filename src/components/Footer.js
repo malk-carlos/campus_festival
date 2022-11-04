5888a8f583c162bec4dfb7969f00eb694620c975
malk-carlos <81245539+malk-carlos@users.noreply.github.com>
2022-11-05 00:06:41 +0900

    update_footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ function Foot() {
     return (
         <footer>
             <h1>Campus Festival 2022 in Hiroshima</h1>
-            <nav>
+            {/* <nav>
                 <ul>
                     <li><a href='#Program'>企画一覧</a></li>
                     <li><a href='#Timetable'>タイムテーブル</a></li>
@@ -20,8 +20,22 @@ function Foot() {
                     <li><a href={f_cf_all} target='_blank' rel='noopener noreferrer'>キャンフェス全体</a></li>
                     <li><a href={f_blog} target='_blank' rel='noopener noreferrer'>ブログ</a></li>
                     <li><a href='../contact.html' target='_blank' rel='noopener noreferrer'>お問い合わせ</a></li>
-                    <li><a href='../contact.html' target='_blank' rel='noopener noreferrer'>お問い合わせ</a></li>
                 </ul>
+            </nav> */}
+            <nav>
+                <div className='foot_div' id='internal'>{/* サイト内遷移 */}
+                    <a href='#Program'>企画一覧</a>
+                    <a href='#Timetable'>タイムテーブル</a>
+                    <a href='#Map'>場内マップ</a>
+                    <a href='#Access'>会場アクセス</a>
+                    <a href='#Rule'>注意事項</a>
+                </div>
+                <div className='foot_div' id='external'>
+                    <a href={f_broadcast} target='_blank' rel='noopener noreferrer'>配信URL</a>
+                    <a href={f_cf_all} target='_blank' rel='noopener noreferrer'>キャンフェス全体</a>
+                    <a href={f_blog} target='_blank' rel='noopener noreferrer'>ブログ</a>
+                    <a href='../contact.html' target='_blank' rel='noopener noreferrer'>お問い合わせ</a>
+                </div>
             </nav>
             {/* <div id='disclaimer'>
                 <p>このサイトは生徒が運営しています。掲載する情報には細心の注意を払っておりますが、より正確な情報は<a href='https://nnn.ed.jp/' target='_blank' rel='noopener noreferrer'>N/S高等学校公式サイト</a>をご覧ください。</p>
